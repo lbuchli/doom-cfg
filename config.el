a1ead7bb577a9c81bd670d99780b6fb384d386b5
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-07-26 22:06:40 +0200

    Change visual appearance

--- config.el
+++ config.el
@@ -1,3 +1,12 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
-;; Place your private configuration here
+(setq
+ +doom-dashboard-banner-dir doom-private-dir
+ +doom-dashboard-banner-file "emacs-logo.png"
+)
+
+;; enable the menu bar
+(menu-bar-mode 1)
+
+;; disable scroll bar
+(toggle-scroll-bar -1)
