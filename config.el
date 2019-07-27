21b2787f670f314df4bed0d38cebf62e5674133e
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-07-27 08:56:47 +0200

    Add basic go IDE capabilities

--- config.el
+++ config.el
@@ -5,5 +5,18 @@
  +doom-dashboard-banner-file "emacs-logo.png"
 )
 
+;; Visuals
 (menu-bar-mode 1)      ;; enable the menu bar
 (toggle-scroll-bar -1) ;; disable scroll bar
+
+;; Go
+(defun cfg-go-mode-hook ()
+  "Hook for go mode"
+  (add-hook 'before-save-hook 'gofmt-before-save) ;; format before saving
+
+  ;; compile & run (SPC c c)
+  (if (not (string-match "go" compile-command))
+      (set (make-local-variable 'compile-command)
+           "go build -v && go test -v && go vet"))
+)
+(add-hook 'go-mode-hook 'cfg-go-mode-hook)
