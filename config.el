426794514ff49c5e4d698d2eb1d197f19177c239
Lukas Buchli <lbu01@buchli-bigger.ch>
2019-08-12 14:24:38 +0200

    Keyboard shortcuts

--- config.el
+++ config.el
@@ -1,13 +1,26 @@
 ;;; ~/.doom.d/config.el -*- lexical-binding: t; -*-
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                               Visuals                                     ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(menu-bar-mode 1)      ;; enable the menu bar
+(toggle-scroll-bar -1) ;; disable scroll bar
+
+;; Start Screen Logo
 (setq
  +doom-dashboard-banner-dir doom-private-dir
  +doom-dashboard-banner-file "emacs-logo.png"
+ )
+
+;; Quit Message
+(setq
+ +doom-quit-messages '("")
 )
 
-;; Visuals
-(menu-bar-mode 1)      ;; enable the menu bar
-(toggle-scroll-bar -1) ;; disable scroll bar
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                              Languages                                    ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Go
 (defun cfg-go-mode-hook ()
@@ -17,6 +30,31 @@
   ;; compile & run (SPC c c)
   (if (not (string-match "go" compile-command))
       (set (make-local-variable 'compile-command)
-           "go build -v && go test -v && go vet"))
+           "go test -v *.go && go vet *.go; echo ; go run *.go"))
 )
 (add-hook 'go-mode-hook 'cfg-go-mode-hook)
+
+;; Verilog
+(setq
+ verilog-simulator "verilator"
+)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;                          Keyboard Shortcuts                               ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; General
+(set-input-mode t nil t "ESC") ;; Quit (C-g) to ESC
+
+;; code
+(map! :leader (:prefix-map ("c" . "code")
+                :desc "Comment out region" "o" #'comment-region
+                :desc "Uncomment region"   "u" #'uncomment-region
+))
+
+;; Tab Bar
+(map! :leader "a" 'tabbar-backward-tab)
+(map! :leader "d" 'tabbar-forward-tab)
+
+(provide 'config)
+;;; config.el ends here
--- packages.el
+++ packages.el
@@ -5,3 +5,6 @@
 ;; (package! some-package)
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
+
+;;(package! tabbar :recipe (:fetcher github :repo "lbuchli/tabbar"))
+(package! verilog-mode :recipe (:fetcher github :repo "veripool/verilog-mode"))
